8b35e08754f5fcad0f292a77b8f2e6000fb9b82d
betaniapereira <betania.pereira@sou.unifeob.edu.br>
2024-11-22 20:56:46 -0300

    fix: remove prefix api unsed

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -5,27 +5,27 @@ const AuthController = require('../controllers/AuthController');
 const verificarAutenticacao = require('../middleware/authMiddleware');  
 
 // Rota para criar um novo usuário
-router.post('/api/user', UsersController.create);
+router.post('/user', UsersController.create);
 
 // Rota para login 
-router.post('/api/login', AuthController.login);
+router.post('/login', AuthController.login);
 
 // Rota para solicitar recuperação de senha
-router.post('/api/password-reset/request', AuthController.requestPasswordReset); 
+router.post('/password-reset/request', AuthController.requestPasswordReset); 
 
 // Rota para redefinir senha
-router.post('/api/password-reset', AuthController.resetPassword); 
+router.post('/password-reset', AuthController.resetPassword); 
 
 // Rota para listar todos os usuários (protegida)
-router.get('/api/users', verificarAutenticacao, UsersController.listUsers);
+router.get('/users', verificarAutenticacao, UsersController.listUsers);
 
 // Rota para listar um usuário específico pelo ID (protegida)
-router.get('/api/user/:id', verificarAutenticacao, UsersController.listUser);
+router.get('/user/:id', verificarAutenticacao, UsersController.listUser);
 
 // Rota para atualizar um usuário específico pelo ID (protegida)
-router.put('/api/user/:id', verificarAutenticacao, UsersController.updateUser);
+router.put('/user/:id', verificarAutenticacao, UsersController.updateUser);
 
 // Rota para deletar um usuário específico pelo ID (protegida)
-router.delete('/api/user/:id', verificarAutenticacao, UsersController.deleteUser);
+router.delete('/user/:id', verificarAutenticacao, UsersController.deleteUser);
 
 module.exports = router;
