0eb96a06e33ac30704aa191639cfe7ea36b66105
betaniapereira <betania.pereira@sou.unifeob.edu.br>
2024-11-27 12:18:08 -0300

    teste cadasro_erro

---
+++ src/models/Cadastro.js
@@ -0,0 +1,34 @@
+const knex = require('../data/conection');
+
+class Cadastro {
+    async create(NOME, ANO, ENDERECO, FELIACAOPAI, FELIACAOMAE, RESPONSAVEL, TELEFONERESPONSAVEL, DATA_MODIFICACAO) {
+        try {
+            await knex.insert({
+                nome: NOME,
+                nascimento: ANO,
+                endereco: ENDERECO,
+                pai: FELIACAOPAI,
+                mae: FELIACAOMAE,
+                responsavel: RESPONSAVEL,
+                telefone: TELEFONERESPONSAVEL,
+                modificacao: DATA_MODIFICACAO
+            }).table('cadastro');
+
+            return 200;
+        } catch (error) {
+            console.log(error);
+            return 404;
+        }
+    }
+
+    async findByUserId(idCADASTRO) {
+        try {
+
+        } catch (error) {
+            console.log(error);
+            return 404;
+        }
+    }
+}
+
+module.exports = new cadastro();
